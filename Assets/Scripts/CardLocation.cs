b545f115374bccf29e3675194ef3ba870add20f9
Cocaine <kokotkin.alexander@gmail.com>
2018-12-22 15:22:19 +0200

    Create CardLocation.cs
    create Propertys

---
+++ Assets/Scripts/CardLocation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardLocation : MonoBehaviour {
+
+    private string name;
+
+    private float offsetX;
+    private float offsetY;
+
+    private float scaleMultiplierX;
+    private float scaleMultiplierY;
+
+    private int cardsGiven;
+
+    public string Name {
+        get { return name; }
+        set { name = value; }
+    }
+    public float OffsetX {
+        get { return offsetX; }
+        set { offsetX = value; }
+    }
+    public float OffsetY {
+        get { return offsetY; }
+        set { offsetY = value; }
+    }
+    public float ScaleMultipillerX {
+        get { return scaleMultiplierX; }
+        set { scaleMultiplierX = value; }
+    }
+    public float ScaleMultipillerY {
+        get { return scaleMultiplierY; }
+        set { scaleMultiplierY = value; }
+    }
+    public int CardsGiven {
+        get { return cardsGiven; }
+        set { cardsGiven = value; }
+    }
+
+
+}
