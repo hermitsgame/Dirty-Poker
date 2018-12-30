1550a6869a61c4d5481861deffed82a206ef2c65
Cocaine <kokotkin.alexander@gmail.com>
2018-12-30 10:39:20 +0200

    Create Trade.cs

---
+++ Assets/Scripts/Logic/Trade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trade : MonoBehaviour, ICardsOpeations {
+
+    [SerializeField] private Card card;
+    [SerializeField] private DeckController deckController;
+
+    private Card[] tradeCards = new Card[5];
+    private int score;
+
+    public Card[] TradeCards {
+        get { return tradeCards; }
+        set { tradeCards = value; }
+    }
+
+    public int Score {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public IEnumerator GetCards(Card[] cards, string cardLocation) {
+        throw new System.NotImplementedException();
+    }
+
+    public void SumScore(int firstCard, int secondCard) {
+        throw new System.NotImplementedException();
+    }
+}
