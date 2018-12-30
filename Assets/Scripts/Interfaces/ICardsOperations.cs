a7f04efb77e75a78e5ae46f01ac4f93b017d4d96
Cocaine <kokotkin.alexander@gmail.com>
2018-12-30 10:38:39 +0200

    Create ICardsOperations.cs

---
+++ Assets/Scripts/Interfaces/ICardsOperations.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICardsOpeations{
+
+    IEnumerator GetCards(Card[] cards, string cardLocation);
+
+    void SumScore(int firstCard, int secondCard);
+
+}
