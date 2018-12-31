96dd847029b3ba2cda692c0999a12aa2a92e350b
Cocaine <kokotkin.alexander@gmail.com>
2018-12-31 08:46:54 +0200

    Create UIController.cs

---
+++ Assets/Scripts/GUI/UIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField] private Text startLabel;
+
+    private bool startGameFlag = false;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(textBlinking(startLabel));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StartTheGame(startLabel);
+    }
+
+    void StartTheGame(Text text) {
+
+        if (Input.GetKeyDown("space") && startGameFlag != true) {
+            Destroy(text.gameObject);
+            startGameFlag = true;
+            Debug.Log("GAME STARTED!");
+        }
+    }
+    IEnumerator textBlinking(Text text) {
+
+            while(startGameFlag != true) {
+                text.text = "";
+                yield return new WaitForSeconds(.5f);
+                text.text = "Press \"SPACE\" button to start!";
+                yield return new WaitForSeconds(.5f);
+            }
+
+    }
+}
