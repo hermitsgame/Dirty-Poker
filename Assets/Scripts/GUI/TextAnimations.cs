258776b6d45073755912abb52222c03df62d2667
Cocaine <kokotkin.alexander@gmail.com>
2019-01-01 11:14:48 +0200

    Create TextAnimations.cs

---
+++ Assets/Scripts/GUI/TextAnimations.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimations : MonoBehaviour {
+
+    [SerializeField] private Text startLabel;
+    [SerializeField] private UIController uiController;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(textBlinking(startLabel, uiController.StartGameFlag));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator textBlinking(Text text, bool startGameFlag) {
+
+        while (startGameFlag != true) {
+            text.text = "";
+            yield return new WaitForSeconds(.5f);
+            text.text = "Press \"SPACE\" button to start!";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
